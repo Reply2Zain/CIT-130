075b0b06c6e6ac314f2e842dd8dd17ff7fb740ec
Reply2Zain <37006843+Reply2Zain@users.noreply.github.com>
2018-10-15 15:48:02 -0400

    Updated 10/15

--- PolishMath/src/polishmath/PolishMath.java
+++ PolishMath/src/polishmath/PolishMath.java
@@ -18,25 +18,42 @@ public class PolishMath {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- // Creating a Stack
-    String s ;
-    int x;
-        Stack <String> polishMath = new Stack<>();
-        Scanner scan = new Scanner(System.in);
+  // Creating a Stack
+    String s = "placeHolder" ;
+    int valueX = 0;
+    int i = -1; //counter
+    int x = 0;
+    
+        Stack <String> stack = new Stack<>();  // create a stack called stack
+        Scanner scan = new Scanner(System.in); //makes a scanner object
         System.out.println("Welcome to Polish Math Comp.");
-        System.out.println("Print out the first value");
+        System.out.println("Type out the list of value");  //Welcome text
+       
+        
+        
+                
+        while (!s.endsWith("=")){
         s = scan.next();
-       x =  Integer.parseInt(s);
+        stack.push(s);
+        i++;
+        }                   // Puts every value into the stack
         
-        System.out.println("String s string is " + s);
-        System.out.println("int x is " + x);
+        stack.pop(); //removes the equal sign from stack
         
-        //polishMath.push("2");
-        //polishMath.push("5");
-  
-        System.out.println(polishMath);   
-    
-    
+        System.out.println("i is equal to " + i);
+        while (x<i) {
+        if (stack.get(x).endsWith("*"))
+            
+        valueX =  Integer.parseInt(stack.get(x));
+        x++;
+        }
+
+        //System.out.println("int x is " + x);
+        //stack.push("2");
+        //stack.push("5");
+        
+        System.out.println("This is the resulting stack");
+        System.out.println(stack);   
     }
     
 }
