82dad607d52a24fdd0ec557096f08b2d3eda168c
Reply2Zain <37006843+Reply2Zain@users.noreply.github.com>
2018-10-15 12:51:06 -0400

    Added and changed a lot
    Now there is a working stack, still needs to work on parseInt

---
+++ PolishMathTest/src/polishmathtest/PolishMathTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polishmathtest;
+
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ *
+ * @author zain.aaban
+ */
+public class PolishMathTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ // Creating a Stack
+    String s = "placeHolder" ;
+    int valueX = 0;
+    int i = -1; //counter
+    int x = 0;
+    
+        Stack <String> stack = new Stack<>();  // create a stack called stack
+        Scanner scan = new Scanner(System.in); //makes a scanner object
+        System.out.println("Welcome to Polish Math Comp.");
+        System.out.println("Type out the list of value");  //Welcome text
+       
+        
+        
+                
+        while (!s.endsWith("=")){
+        s = scan.next();
+        stack.push(s);
+        i++;
+        }                   // Puts every value into the stack
+        
+        stack.pop(); //removes the equal sign from stack
+        
+        while (x<=i) {
+        valueX =  Integer.parseInt(stack.get(x));
+            System.out.println("#"+x+ "is " + valueX);
+        x++;
+        }
+
+        //System.out.println("int x is " + x);
+        //stack.push("2");
+        //stack.push("5");
+  
+        System.out.println(stack);   
+    
+        }
+    
+}
+
