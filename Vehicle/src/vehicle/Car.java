26c11ac2de3c5ed98f931e71e915bed9ca854752
Reply2Zain <37006843+Reply2Zain@users.noreply.github.com>
2018-10-17 15:56:31 -0400

    Add files via upload

---
+++ Vehicle/src/vehicle/Car.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicle;
+
+/**
+ *
+ * @author zain.aaban
+ */
+
+
+public class Car extends Vehicle {
+    int numOfDoors;
+    int currentGear ;
+    double totalMiles;
+ 
+    
+   public Car(int numDoor, int ms, int year, String ftype){
+
+
+}
+    
+}
+
+
+
+
+
---
+++ Vehicle/src/vehicle/Vehicle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicle;
+
+/**
+ *
+ * @author zain.aaban
+ */
+
+
+public class Vehicle  {
+    int speed;
+    int maxSpeed;
+    int yearOfOrigin;
+    String fuelType;
+  
+    public Vehicle(){
+        System.out.println("hi");
+    }
+    public Vehicle(int ms, int year, String ftype){
+        
+         System.out.println("");
+        
+        
+        
+        }
+    }
+
+
+
