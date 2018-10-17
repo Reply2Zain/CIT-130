41aad758bdcc9c2f4ee5bfb8bc37009d21aa76e1
Reply2Zain <37006843+Reply2Zain@users.noreply.github.com>
2018-10-17 14:44:58 -0400

    Added operator function w/ ifs
    Still need to figure out how to calculate properly with 3 consecutive numbers.

--- PolishMathTest/src/polishmathtest/PolishMathTest.java
+++ PolishMathTest/src/polishmathtest/PolishMathTest.java
@@ -3,15 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package polishmathtest;
+package stack;
 
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.Stack;
-
 /**
  *
- * @author zain.aaban
+ * @author reply
  */
 public class PolishMathTest {
 
@@ -19,13 +17,14 @@ public class PolishMathTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- // Creating a Stack
+  // Creating a Stack
     String s = "placeHolder" ;
-    int valueX = 0;
-    int i = -1; //counter
-    int x = 0;
+    int total = 0;
+    int next = 0;
+    int stackSize = -1; //counter
+    int stackTicker = 0;
     
-        Stack <String> stack = new Stack<>();  // create a stack called stack
+        Stack <String> stack = new Stack <>();  // create a stack called stack
         Scanner scan = new Scanner(System.in); //makes a scanner object
         System.out.println("Welcome to Polish Math Comp.");
         System.out.println("Type out the list of value");  //Welcome text
@@ -36,16 +35,37 @@ public class PolishMathTest {
         while (!s.endsWith("=")){
         s = scan.next();
         stack.push(s);
-        i++;
+        stackSize++;           // i counts the number of values in the stack 
         }                   // Puts every value into the stack
         
         stack.pop(); //removes the equal sign from stack
         
-        System.out.println("i is equal to " + i);
-        while (x<i) {
-        valueX =  Integer.parseInt(stack.get(x));
-            System.out.println("#"+x+ " is " + valueX);
-        x++;
+        
+        total =  Integer.parseInt(stack.get(stackTicker));
+        stackTicker++;      // puts the top value into total and counts the stackTicker
+        
+        
+        System.out.println("The stack size is " + stackSize); // total size of stack
+        while (stackTicker<stackSize) {     //stackTicker starts at 0
+        if (stack.get(stackTicker).equalsIgnoreCase("*"))
+            total*=next
+                    ;
+        
+        if (stack.get(stackTicker).equalsIgnoreCase("/")){
+            total/=next;
+            //stack.pop();
+        }
+        if (stack.get(stackTicker).equalsIgnoreCase("+"))
+            total+=next
+            ;
+        if (stack.get(stackTicker).equalsIgnoreCase("-"))
+            total-=next
+            ;
+        
+        else
+            next =  Integer.parseInt(stack.get(stackTicker));
+        
+        stackTicker++;
         }
 
         //System.out.println("int x is " + x);
@@ -53,9 +73,6 @@ public class PolishMathTest {
         //stack.push("5");
         
         System.out.println("This is the resulting stack");
-        System.out.println(stack);   
-    
-        }
+        System.out.println(stack);       }
     
 }
-
